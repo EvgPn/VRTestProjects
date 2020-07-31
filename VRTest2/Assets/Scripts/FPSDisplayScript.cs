85fed425f3697c973346551d14c5bf489b656bfb
EvgPn <47789792+EvgPn@users.noreply.github.com>
2020-07-31 16:37:30 +0300

    feat(project) add fps counter on left hand

---
+++ VRTest2/Assets/Scripts/FPSDisplayScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplayScript : MonoBehaviour
+{
+	[SerializeField] private Text _fpsText;
+
+	private void Update()
+	{
+		float current = (int)(1f / Time.deltaTime);
+		if(Time.frameCount % 50 == 0)
+		{
+			_fpsText.text = "FPS " + current.ToString();
+		}
+	}
+}
