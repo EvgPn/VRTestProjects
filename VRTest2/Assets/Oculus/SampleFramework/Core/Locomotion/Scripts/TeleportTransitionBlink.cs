6f32eb89fb40c93d6831f7690fb2d8d23a10d3a0
EvgPn <47789792+EvgPn@users.noreply.github.com>
2020-07-31 13:04:24 +0300

    fix(teleport) add teleport blink

--- VRTest2/Assets/Oculus/SampleFramework/Core/Locomotion/Scripts/TeleportTransitionBlink.cs
+++ VRTest2/Assets/Oculus/SampleFramework/Core/Locomotion/Scripts/TeleportTransitionBlink.cs
@@ -17,6 +17,8 @@ using System.Collections;
 /// </summary>
 public class TeleportTransitionBlink : TeleportTransition
 {
+	[SerializeField] private OVRScreenFade _fader; 
+
 	/// <summary>
 	/// How long the transition takes. Usually this is greater than Teleport Delay.
 	/// </summary>
@@ -66,11 +68,11 @@ public class TeleportTransitionBlink : TeleportTransition
 				teleported = true;
 				LocomotionTeleport.DoTeleport();
 			}
-			//float fadeLevel = FadeLevels.Evaluate(elapsedTime / TransitionDuration);
-			//OVRInspector.instance.fader.SetFadeLevel(fadeLevel);
+			float fadeLevel = FadeLevels.Evaluate(elapsedTime / TransitionDuration);
+			_fader.SetFadeLevel(fadeLevel);
 		}
 
-		//OVRInspector.instance.fader.SetFadeLevel(0);
+		_fader.SetFadeLevel(0);
 
 		LocomotionTeleport.IsTransitioning = false;
 	}
